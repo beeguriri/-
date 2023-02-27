3a335141af27b57010e3d1d2cb41a1f130f870d6
beeguriri <kkamboting@gmail.com>
2023-02-27 14:28:18 +0900

    0227_heap,merge sort 파일 하나로 합치기

--- Lec06_Sort/MinHeap.java
+++ Lec06_Sort/MinHeap.java
@@ -1,5 +1,7 @@
 package Lec06_Sort;
 
+import java.util.Scanner;
+
 /* Heap
  * 배열을 사용하며, i=1부터 사용
  * parent 노드에서 child 노드 : 2*i (좌), 2*i + 1 (우)
@@ -117,13 +119,75 @@ public class MinHeap implements MinPQ {	//interface구현
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int select = 0;
+		Scanner stdIn = new Scanner(System.in);
+		MinHeap heap = new MinHeap(10);			//힙 사이즈 생성자
+		Element ele = null;
+		final int count = 5;
+	    int[] x = new int[count];
+
+		Element deletedEle = null;
+
+		do {
+			System.out.println("Max Tree. Select: 1 insert, 2 display, 3 delete,  4 sort, 5 exit => ");
+			select = stdIn.nextInt();
+			switch (select) {
+			
+			//insert : 갯수만큼 난수 생성해서 heap 만들어짐 (count=5 이므로 난수 5개 생성)
+			case 1:
+			     for (int i = 0; i < count; i++) {
+			         x[i] = (int)(Math.random() * 30);
+						heap.Insert(new Element(x[i]));
+			     }
+				break;
+				
+			case 2:
+				heap.display();
+				break;
+				
+			//delete	
+			case 3:
+				deletedEle = heap.DeleteMin(ele);
+				if (deletedEle != null) {
+					System.out.println("deleted element: " + deletedEle.key);
+				}
+				System.out.println("current max heap: ");
+				heap.display();
+				break;
+			
+			//sort
+			case 4:
+				for (int j = 0; j < count; j++) {
+					deletedEle = heap.DeleteMin(ele);
+					x[j] = deletedEle.getElement();
+				}
+				for (int num: x)
+					System.out.println(" " + num);
+				
+			case 5:
+				return;
+
+			}
+		} while (select < 5);
+
+		return;
+		
 	}
 
 }
 
+class Element {
+	public int key;
 
+	public int getElement() {
+		return key;
+	}
+
+	public Element(int key) {
+		this.key = key;
+	}
+}
 
 
 
